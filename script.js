ef38197dba91741543f1a22c9dc500b3090c0bb3
adamzahrannnn <68937792+adamzahrannnn@users.noreply.github.com>
2021-10-04 06:49:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,13 @@ var hasil = ''
 // alur
 if ( comp == p ){hasil = 'SERI!';}
 //  JEMPOL   
-    else if ( comp == "Jempol"){hasil = ( p == "Telunjuk" ) ? "KALAH!" : "MENANG!";}
-     else ( comp == "Jempol"){hasil = ( p == "Kelingking" ) ? "MENANG!" : "KALAH!";
-} 
-} 
+    else if ( comp == "Jempol"){hasil = ( p == "Telunjuk" ) ? "KALAH!" : "MENANG!"; } 
 //  TELUNJUK
-    else if ( comp == "Telunjuk"){hasil = ( p == "Jempol" ) ? "MENANG!": "KALAH!";}
-  else if ( comp == "Telunjuk"){hasil = ( p == "Kelingking" ) ? "KALAH!": "MENANG!";           
-} 
+    else if ( comp == "Telunjuk"){hasil = ( p == "Jempol" ) ? "MENANG!": "KALAH!";}          
+
 // Kelingking
     else if ( comp == "Kelingking"){ hasil = ( p == "Telunjuk" ) ? "MENANG!" : "KALAH!";}
-    else if ( comp == "Kelingking"){ hasil = ( p == "Jempol" ) ? "KALAH!" : "MENANG!";
-} else {
+else {
     hasil = "ISI YANG BENER JINGS"
 }
 
